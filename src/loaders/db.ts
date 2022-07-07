454a06816b9de1aa4b2988f2fe430d764770ffd1
hyejungg <kimhyejung12@naver.com>
2022-07-07 19:14:13 +0900

    #9 [fix] 기존 console.log -> logger 사용하도록 변경

--- src/loaders/db.ts
+++ src/loaders/db.ts
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import config from '../config';
+import { logger } from '../config/logger';
 import Check from '../models/Check';
 import Event from '../models/Event';
 import Room from '../models/Room';
@@ -14,31 +15,31 @@ const connectDB = async () => {
 
     mongoose.set('autoCreate', true);
 
-    console.log('Mongoose Connected ...');
+    logger.info('Mongoose Connected ...');
 
     User.createCollection().then(function (collection) {
-      console.log('User Collection is created!');
+      logger.info('User Collection is created!');
     });
     Type.createCollection().then(function (collection) {
-      console.log('Type Collection is created!');
+      logger.info('Type Collection is created!');
     });
     RuleCategory.createCollection().then(function (collection) {
-      console.log('RuleCategory Collection is created!');
+      logger.info('RuleCategory Collection is created!');
     });
     Rule.createCollection().then(function (collection) {
-      console.log('Rule Collection is created!');
+      logger.info('Rule Collection is created!');
     });
     Room.createCollection().then(function (collection) {
-      console.log('Room Collection is created!');
+      logger.info('Room Collection is created!');
     });
     Event.createCollection().then(function (collection) {
-      console.log('Event Collection is created!');
+      logger.info('Event Collection is created!');
     });
     Check.createCollection().then(function (collection) {
-      console.log('Check Collection is created!');
+      logger.info('Check Collection is created!');
     });
   } catch (err: any) {
-    console.error(err.message);
+    logger.error(err.message);
     process.exit(1);
   }
 };

e8076366e9f2ba1d6798bba29f1c76525e6e90c7
hyejungg <kimhyejung12@naver.com>
2022-07-08 04:38:49 +0900

    #16 [feat] 나의 프로필 조회 api 구현

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -2,10 +2,12 @@ import bcrypt from 'bcryptjs';
 import errorGenerator from '../errors/errorGenerator';
 import { SignupDto } from '../interfaces/auth/SignupDto';
 import { PostBaseResponseDto } from '../interfaces/common/PostBaseResponseDto';
+import { UserResponseDto } from '../interfaces/user/UserResponseDto';
 import { UserUpdateDto } from '../interfaces/user/UserUpdateDto';
 import User from '../models/User';
 import message from '../modules/responseMessage';
 import statusCode from '../modules/statusCode';
+import UserServiceUtils from './UserServiceUtils';
 
 const createUser = async (
   signupDto: SignupDto
@@ -45,13 +47,42 @@ const createUser = async (
   }
 };
 
+const selectUser = async (userId: string): Promise<UserResponseDto[]> => {
+  try {
+    await UserServiceUtils.findUserById(userId);
+
+    const userInfo = await User.find({
+      _id: userId
+    }).populate('typeId', 'typeName typeColor');
+
+    if (!userInfo) throw errorGenerator({ statusCode: statusCode.NOT_FOUND });
+
+    const data: UserResponseDto[] = userInfo.map((user: any) => {
+      const result = {
+        userName: user.userName,
+        job: user.job,
+        introduction: user.introduction,
+        hashTag: user.hashTag,
+        typeName: user.typeId.typeName,
+        typeColor: user.typeId.typeColor,
+        typeScore: user.typeScore,
+        notificationState: user.notificationState
+      };
+      return result;
+    });
+
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
+
 const updateUser = async (
   userId: string,
   userUpdateDto: UserUpdateDto
 ): Promise<void> => {
   try {
-    const user = await User.findById(userId);
-    if (!user) throw errorGenerator({ statusCode: statusCode.UNAUTHORIZED });
+    await UserServiceUtils.findUserById(userId);
 
     await User.findByIdAndUpdate(userId, userUpdateDto);
   } catch (error) {
@@ -61,5 +92,6 @@ const updateUser = async (
 
 export default {
   createUser,
-  updateUser
+  updateUser,
+  selectUser
 };

1cc83e28a978bcc069d6cec49c95fb4efac6fa4f
Yunjoo Son <thsduswn4141@naver.com>
2022-07-08 15:00:19 +0900

    #13 [refactor] checkObjectIdValidation.ts 동작 방식 수정

--- src/modules/checkObjectIdValidation.ts
+++ src/modules/checkObjectIdValidation.ts
@@ -1,10 +1,19 @@
 import mongoose from 'mongoose';
+import errorGenerator from '../errors/errorGenerator';
+import message from '../modules/responseMessage';
+import statusCode from '../modules/statusCode';
 
 const ObjectId = mongoose.Types.ObjectId;
-export const isValidObjectId = (id: string): boolean => {
+export const checkObjectIdValidation = (id: string): void => {
   if (ObjectId.isValid(id)) {
-    if (String(new ObjectId(id)) === id) return true;
-    return false;
+    if (String(new ObjectId(id)) === id) return;
+    throw errorGenerator({
+      msg: message.INVALID_ID,
+      statusCode: statusCode.BAD_REQUEST
+    });
   }
-  return false;
+  throw errorGenerator({
+    msg: message.INVALID_ID,
+    statusCode: statusCode.BAD_REQUEST
+  });
 };

025b020f23fdcae2100f90b07ee8e5ce58dc2bf8
hyejungg <kimhyejung12@naver.com>
2022-07-09 02:18:44 +0900

    #38 [fix] 사용자 정보 수정 api 수정

--- src/routes/UserRouter.ts
+++ src/routes/UserRouter.ts
@@ -2,13 +2,23 @@ import { Router } from 'express';
 import { body } from 'express-validator/check';
 import { UserController } from '../controllers';
 import auth from '../middleware/auth';
+import limitNum from '../modules/limitNum';
 
 const router: Router = Router();
 
 router.get('/profile/me', auth, UserController.getUser);
 router.put(
   '/profile/me',
-  [body('userName').notEmpty()],
+  [
+    body('userName')
+      .not()
+      .isEmpty()
+      .isLength({ max: limitNum.PROFILE_NAME_MAX_LENGTH }),
+    body('job').isLength({ max: limitNum.PROFILE_JOB_MAX_LENGTH }),
+    body('introduction').isLength({
+      max: limitNum.PROFILE_INTRODUCTION_MAX_LENGTH
+    })
+  ],
   auth,
   UserController.updateUser
 );

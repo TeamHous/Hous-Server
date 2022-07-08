24f33cc69e0794410abebcbff61adc78fe9613be
HyeokJoon Kong <orijoon98@gmail.com>
2022-07-09 03:20:51 +0900

    #36 [fix] 코리 반영

--- src/services/RoomServiceUtils.ts
+++ src/services/RoomServiceUtils.ts
@@ -5,11 +5,12 @@ import statusCode from '../modules/statusCode';
 
 const findUserById = async (userId: string) => {
   const user = await User.findById(userId);
-  if (!user)
+  if (!user) {
     throw errorGenerator({
       msg: message.UNAUTHORIZED,
       statusCode: statusCode.UNAUTHORIZED
     });
+  }
   return user;
 };
 

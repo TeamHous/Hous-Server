9914252e9a8847f05d4170243c6423eea19054d4
hyejungg <kimhyejung12@naver.com>
2022-07-10 01:34:37 +0900

    #46 [feat] 이벤트 수정하기 api 구현

--- src/controllers/EventController.ts
+++ src/controllers/EventController.ts
@@ -1,7 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
 import { Result, ValidationError, validationResult } from 'express-validator';
 import { EventCreateDto } from '../interfaces/event/EventCreateDto';
-import isIconType from '../modules/isIconType';
+import { EventUpdateDto } from '../interfaces/event/EventUpdateDto';
+import checkIconType from '../modules/checkIconType';
 import message from '../modules/responseMessage';
 import statusCode from '../modules/statusCode';
 import util from '../modules/util';
@@ -31,7 +32,7 @@ const createEvent = async (
   const { roomId } = req.params;
 
   try {
-    isIconType(eventCreateDto.eventIcon);
+    checkIconType.isEventIconType(eventCreateDto.eventIcon);
 
     const result = await EventService.createEvent(
       userId,
@@ -49,4 +50,47 @@ const createEvent = async (
   }
 };
 
-export default { createEvent };
+/**
+ *  @route PUT /room/:roomId/event/:eventId
+ *  @desc Update Event
+ *  @access private
+ */
+const updateEvent = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void | Response> => {
+  const errors: Result<ValidationError> = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res
+      .status(statusCode.BAD_REQUEST)
+      .send(
+        util.fail(statusCode.BAD_REQUEST, message.BAD_REQUEST, errors.array())
+      );
+  }
+
+  const userId: string = req.body.user._id;
+  const eventUpdateDto: EventUpdateDto = req.body;
+  const { roomId, eventId } = req.params;
+
+  try {
+    checkIconType.isEventIconType(eventUpdateDto.eventIcon);
+
+    const result = await EventService.updateEvent(
+      userId,
+      roomId,
+      eventId,
+      eventUpdateDto
+    );
+
+    return res
+      .status(statusCode.CREATED)
+      .send(
+        util.success(statusCode.CREATED, message.CREATE_EVENT_SUCCESS, result)
+      );
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default { createEvent, updateEvent };

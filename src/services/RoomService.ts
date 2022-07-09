e235cc12fc2f467b5a45b9cbb9f331ec59e88041
HyeokJoon Kong <orijoon98@gmail.com>
2022-07-10 01:27:29 +0900

    #45 [fix] 방 생성 api 수정

--- src/services/RoomService.ts
+++ src/services/RoomService.ts
@@ -3,7 +3,9 @@ import { PostBaseResponseDto } from '../interfaces/common/PostBaseResponseDto';
 import { RoomJoinDto } from '../interfaces/room/RoomJoinDto';
 import { RoomJoinResponseDto } from '../interfaces/room/RoomJoinResponseDto';
 import { RoomResponseDto } from '../interfaces/room/RoomResponseDto';
+import Event from '../models/Event';
 import Room from '../models/Room';
+import RuleCategory from '../models/RuleCategory';
 import User from '../models/User';
 import checkObjectIdValidation from '../modules/checkObjectIdValidation';
 import message from '../modules/responseMessage';
@@ -29,10 +31,26 @@ const createRoom = async (userId: string): Promise<RoomResponseDto> => {
     await room.save();
 
     await User.findByIdAndUpdate(userId, {
-      $set: {
-        roomId: room._id
-      }
+      roomId: room._id
     });
+
+    const ruleCategory = new RuleCategory({
+      roomId: room._id,
+      categoryName: '청소',
+      categoryIcon: 'CLEAN'
+    });
+
+    await ruleCategory.save();
+
+    const event = new Event({
+      roomId: room._id,
+      eventName: '여기에 이벤트를 추가하세요.',
+      eventIcon: 'PARTY',
+      date: Date.now() + 10 * 24 * 60 * 60 * 1000, // 오늘 + 10일
+      participantsId: [user._id]
+    });
+
+    await event.save();
 
     const data: RoomResponseDto = {
       _id: room._id,

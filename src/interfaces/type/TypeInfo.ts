c678e45908afd627804d113fb2aaf1e0e1f6e2e6
Yunjoo Son <thsduswn4141@naver.com>
2022-07-16 06:24:51 +0900

    #131 [fix] Type 컬렉션 수정

--- src/interfaces/type/TypeInfo.ts
+++ src/interfaces/type/TypeInfo.ts
@@ -1,4 +1,16 @@
 export interface TypeInfo {
   typeName: string;
   typeColor: string;
+  typeImg: string;
+  typeOneComment: string;
+  typeDesc: string;
+  typeRulesTitle: string;
+  typeRules: string[];
+  good: Compatibility[];
+  bad: Compatibility[];
+}
+
+interface Compatibility {
+  typeName: string;
+  typeImg: string;
 }
--- src/models/Type.ts
+++ src/models/Type.ts
@@ -12,6 +12,52 @@ const TypeSchema = new mongoose.Schema(
       type: String,
       required: true,
       unique: true
+    },
+    typeImg: {
+      type: String,
+      required: true
+    },
+    typeOneComment: {
+      type: String,
+      required: true
+    },
+    typeDesc: {
+      type: String,
+      required: true
+    },
+    typeRulesTitle: {
+      type: String,
+      required: true
+    },
+    typeRules: [
+      {
+        type: String,
+        required: true
+      }
+    ],
+    good: {
+      goodName: {
+        type: String,
+        required: true
+      },
+      goodImg: [
+        {
+          type: String,
+          required: true
+        }
+      ]
+    },
+    bad: {
+      badName: {
+        type: String,
+        required: true
+      },
+      badImg: [
+        {
+          type: String,
+          required: true
+        }
+      ]
     }
   },
   {

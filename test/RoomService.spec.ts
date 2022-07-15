6cc7e13508604480d35536248542f99ba213acc3
HyeokJoon Kong <orijoon98@gmail.com>
2022-07-15 15:20:10 +0900

    #115 [test] 방 퇴사 성공 케이스 테스트

--- test/RoomService.spec.ts
+++ test/RoomService.spec.ts
@@ -87,4 +87,26 @@ describe('RoomService Tests', () => {
     assert.equal(user!.roomId.toString(), createdRoomId);
     assert.equal(user!.roomId.toString(), result._id.toString());
   });
+  it('leaveRoom test', async () => {
+    // given
+    const signupDto1: SignupDto = {
+      email: 'test1@gmail.com',
+      password: 'password',
+      userName: '테스트유저',
+      gender: '남자',
+      fcmToken: '테스트토큰'
+    };
+    const userId1: string = (
+      await UserService.createUser(signupDto1)
+    )._id.toString();
+    const createdRoom: RoomResponseDto = await RoomService.createRoom(userId1);
+    const createdRoomId: string = createdRoom._id.toString();
+
+    // when
+    await RoomService.leaveRoom(userId1, createdRoomId);
+
+    // then
+    const user = await User.findById(userId1);
+    assert.equal(user!.roomId, null);
+  });
 });

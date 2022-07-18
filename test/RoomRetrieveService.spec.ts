8bd61874b1e63d0e91289699628b9d161aa5f84c
HyeokJoon Kong <orijoon98@gmail.com>
2022-07-19 00:09:01 +0900

    #159 [test] getRoomAndUserByRoomCode 테스트 추가

--- test/RoomRetrieveService.spec.ts
+++ test/RoomRetrieveService.spec.ts
@@ -2,6 +2,7 @@ import assert from 'assert';
 import { afterEach } from 'mocha';
 import mongoose from 'mongoose';
 import { SignupDto } from '../src/interfaces/auth/request/SignupDto';
+import { RoomJoinResponseDto } from '../src/interfaces/room/response/RoomJoinResponseDto';
 import { RoomResponseDto } from '../src/interfaces/room/response/RoomResponseDto';
 import Event from '../src/models/Event';
 import Room from '../src/models/Room';
@@ -40,5 +41,44 @@ describe('RoomRetrieveService Tests', () => {
 
     // then
     assert.equal(response!.toString(), createdRoomId);
+  });
+  it('getRoomAndUserByRoomCode test', async () => {
+    // given
+    const signupDto1: SignupDto = {
+      email: 'test1@gmail.com',
+      password: 'password',
+      userName: '테스트유저',
+      gender: '남자',
+      fcmToken: '테스트토큰'
+    };
+    const userId1: string = (
+      await UserService.createUser(signupDto1)
+    )._id.toString();
+    const signupDto2: SignupDto = {
+      email: 'test2@gmail.com',
+      password: 'password',
+      userName: '테스트유저',
+      gender: '남자',
+      fcmToken: '테스트토큰'
+    };
+    const userId2: string = (
+      await UserService.createUser(signupDto2)
+    )._id.toString();
+    const createdRoom: RoomResponseDto = await RoomService.createRoom(userId1);
+    const createdRoomId: string = createdRoom._id.toString();
+    const createdRoomCode: string = createdRoom.roomCode;
+
+    // when
+    const response: RoomJoinResponseDto =
+      await RoomRetrieveService.getRoomAndUserByRoomCode(
+        userId2,
+        createdRoomCode
+      );
+
+    // then
+    assert.equal(response._id.toString(), createdRoomId);
+    assert.equal(response.typeColor, 'GRAY');
+    assert.equal(response.userName, '테스트유저');
+    assert.equal(response.introduction, '');
   });
 });

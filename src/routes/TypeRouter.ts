0206763f38450e2c9ce2c15df4012029009de823
Yunjoo Son <thsduswn4141@naver.com>
2022-07-19 19:35:50 +0900

    #148 [chore] Swagger: TypeRouter - Response

--- src/routes/TypeRouter.ts
+++ src/routes/TypeRouter.ts
@@ -10,6 +10,31 @@ router.get(
   TypeController.getTypeTestInfo
   // #swagger.summary = '성향 테스트 문제 조회'
   // #swagger.security = [{"JWT": []}]
+  /*
+  #swagger.responses[200] = {
+        schema: {
+          status: 200,
+          success: true,
+          message: "사용자 성향 테스트 조회 성공입니다.",
+          data: {
+              typeTests: [
+                  {
+                      _id: "62d298e3cc1d0ea0fc0c4b57",
+                      testNum: 1,
+                      question: "알람 소리에 눈을 뜬 당신\n방 안은 어떤가요?",
+                      questionType: "LIGHT",
+                      answers: [
+                          "스탠드를 키고 자 방이 매우 밝다",
+                          "햇빛이 비춰 자연스럽게 방을 밝힌다",
+                          "암막 커튼이 쳐져있어, 완전히 어둡다"
+                      ],
+                      questionImg: "https://team-hous.s3.ap-northeast-2.amazonaws.com/Type/test/type_test_1.png"
+                  }
+              ]
+          }
+      }
+    }
+  */
 );
 
 export default router;

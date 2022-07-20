1f98e321ad3c20bb51f13f30ec9ac62400a57d33
Yunjoo Son <thsduswn4141@naver.com>
2022-07-20 22:49:03 +0900

    #186 [test] 성향 테스트 조회 test 코드 수정

--- test/TypeRetrieveService.spec.ts
+++ test/TypeRetrieveService.spec.ts
@@ -4,12 +4,15 @@ import { TypeTestInfoResponseDto } from '../src/interfaces/type/response/TypeTes
 import UserService from '../src/services/user/UserService';
 import TypeRetrieveService from '../src/services/type/TypeRetrieveService';
 import { TypeTestInfo } from '../src/interfaces/type/TypeTestInfo';
+import User from '../src/models/User';
 
 describe('TypeRetrieveService Tests', () => {
   // 단위 테스트 종료될 때마다 서비스 관련 컬렉션 초기화
-  let userId1: string = '';
-
-  beforeEach(async () => {
+  afterEach(async () => {
+    await User.collection.drop();
+  });
+  it('getTypeTestInfo test', async () => {
+    //given
     const signupDto1: SignupDto = {
       email: 'test1@gmail.com',
       password: 'password',
@@ -18,9 +21,10 @@ describe('TypeRetrieveService Tests', () => {
       fcmToken: '테스트토큰'
     };
 
-    userId1 = (await UserService.createUser(signupDto1))._id.toString();
-  });
-  it('getTypeTestInfo test', async () => {
+    const userId1: string = (
+      await UserService.createUser(signupDto1)
+    )._id.toString();
+
     // when
     const response: TypeTestInfoResponseDto =
       await TypeRetrieveService.getTypeTestInfo(userId1);

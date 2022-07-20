da8eaece47dfc0f3f1c22b0e2886a97079191786
Yunjoo Son <thsduswn4141@naver.com>
2022-07-21 04:37:31 +0900

    #191 [test] 규칙 카테고리 생성 테스트

--- test/RuleService.spec.ts
+++ test/RuleService.spec.ts
@@ -5,6 +5,8 @@ import { RoomResponseDto } from '../src/interfaces/room/response/RoomResponseDto
 import { RuleCreateDto } from '../src/interfaces/rule/request/RuleCreateDto';
 import { RuleUpdateDto } from '../src/interfaces/rule/request/RuleUpdateDto';
 import { RuleResponseDto } from '../src/interfaces/rule/response/RuleResponseDto';
+import { RuleCategoryCreateDto } from '../src/interfaces/rulecategory/request/RuleCategoryCreateDto';
+import { RuleCategoryResponseDto } from '../src/interfaces/rulecategory/response/RuleCategoryResponseDto';
 import Event from '../src/models/Event';
 import Room from '../src/models/Room';
 import Rule from '../src/models/Rule';
@@ -144,5 +146,38 @@ describe('RuleService Tests', () => {
     // then
     const deletedRule = await Rule.findById(createdRule._id);
     assert.equal(deletedRule, null);
+  });
+  it('createRuleCategory test', async () => {
+    // given
+    const signupDto1: SignupDto = {
+      email: 'test1@gmail.com',
+      password: 'password',
+      userName: '테스트유저',
+      gender: '남자',
+      fcmToken: '테스트토큰'
+    };
+    const userId1: string = (
+      await UserService.createUser(signupDto1)
+    )._id.toString();
+    const createdRoom: RoomResponseDto = await RoomService.createRoom(userId1);
+    const createdRoomId: string = createdRoom._id.toString();
+    const ruleCategoryCreateDto: RuleCategoryCreateDto = {
+      categoryName: '청소 테스트 카테고리',
+      categoryIcon: 'CLEAN'
+    };
+
+    // when
+    const ruleCategory: RuleCategoryResponseDto =
+      await RuleService.createRuleCategory(
+        userId1,
+        createdRoomId,
+        ruleCategoryCreateDto
+      );
+
+    // then
+    assert.equal(ruleCategory.roomId.toString(), createdRoomId);
+    assert.equal(ruleCategory.ruleCategoryName, '청소 테스트 카테고리');
+    assert.equal(ruleCategory.ruleCategoryIcon, 'CLEAN');
+    assert.equal(ruleCategory.ruleCnt, 0);
   });
 });
